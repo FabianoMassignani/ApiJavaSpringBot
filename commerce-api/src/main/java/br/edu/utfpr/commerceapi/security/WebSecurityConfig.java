8c67f3f68df86b17a107ac88219a0ecc3661e878
Fabiano <fabiano-smi@hotmail.com>
2023-09-27 18:54:27 -0300

    #main

--- commerce-api/src/main/java/br/edu/utfpr/commerceapi/security/WebSecurityConfig.java
+++ commerce-api/src/main/java/br/edu/utfpr/commerceapi/security/WebSecurityConfig.java
@@ -1,20 +1,24 @@
 package br.edu.utfpr.commerceapi.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -28,11 +32,15 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Enable CORS and disable CSRF
-        http = http.cors().and().csrf().disable();
-
-        // Set session management to stateless
-        http = http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // 1) Disable CSRF, 2) Enable CORS and 3) Set session management to stateless
+        http = http
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF
+                .cors(Customizer.withDefaults()) // Enable CORS
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Set
+                                                                                                                     // session
+                                                                                                                     // management
+                                                                                                                     // to
+                                                                                                                     // stateless
 
         // Set unauthorized requests exception handler
         http = http
@@ -45,10 +53,14 @@ public class WebSecurityConfig {
                                 }));
 
         // Set permissions on endpoints
-        http.authorizeHttpRequests()
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/api-docs/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth").permitAll()
                 .requestMatchers(HttpMethod.POST, "/person").permitAll()
-                .anyRequest().authenticated();
+                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/index.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                .anyRequest().authenticated());
 
         // Add JWT token filter
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -67,22 +79,42 @@ public class WebSecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
-    // // Método ignora URL específicas
+    // Used by Spring Security if CORS is enabled.
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
     // @Bean // Ignora URL's específicas
     // public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
     // return web -> web.ignoring().requestMatchers("/auth");
     // }
 
     // Used by Spring Security if CORS is enabled.
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        //config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+    /*
+     * @Bean
+     * public CorsFilter corsFilter() {
+     * UrlBasedCorsConfigurationSource source = new
+     * UrlBasedCorsConfigurationSource();
+     * CorsConfiguration config = new CorsConfiguration();
+     * 
+     * // config.setAllowCredentials(true);
+     * config.addAllowedOrigin("*");
+     * config.addAllowedHeader("*");
+     * config.addAllowedMethod("*");
+     * source.registerCorsConfiguration("/**", config);
+     * 
+     * System.out.println("-------------");
+     * System.out.println(config);
+     * 
+     * return new CorsFilter(source);
+     * }
+     */
 }
